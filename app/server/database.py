773fb9adea0f8a4cbd497586f7432239707cc913
Olya Averina <averinaos@inbox.ru>
2021-11-23 19:57:34 +0300

    Добавлены эндпоинты для 3х кейсов: - Список марок - Список моделей определенной марки - Список поколений определенной модели

--- app/server/database.py
+++ app/server/database.py
@@ -39,6 +39,38 @@ async def add_car(car_data: dict) -> dict:
     return car_helper(new_car)
 
 
+# Получить все автомобили, присутствующие в базе данных
+async def retrieve_cars_brands():
+    cars = []
+    async for car in car_collection.find():
+        cars.append(car["brand"])
+    return list(set(cars))
+
+# Получить автомобили соответствующей марки
+async def retrieve_car_on_brand(brand: str) -> list:
+    car = await car_collection.find_one({"brand": brand})
+    if car:
+        arr = []
+        for item in car['models']:
+            model = {'modelName': item['modelName'],
+                     'firstYearProduction': item['firstYearProduction'],
+                     'engineType': item['engineType'],
+                     'style': item['style'],
+                     'generations_count': len(item['generations']),
+                     }
+            arr.append(model)
+        return arr
+
+
+# Получить автомобили соответствующей марки
+async def retrieve_generations_car(brand: str, model_name: str) -> list:
+    data = await car_collection.find_one({'brand': brand, 'models': {'$elemMatch': {'modelName': model_name}}},
+                                         {'models.$': 1})
+    if data:
+        model_data = data['models'][0]
+        if'generations' in model_data:
+            return model_data['generations']
+
 # Получить автомобиль с соответствующим ID
 async def retrieve_car(id: str) -> dict:
     car = await car_collection.find_one({"_id": ObjectId(id)})
--- app/server/routes/car.py
+++ app/server/routes/car.py
@@ -5,7 +5,10 @@ from app.server.database import (
     add_car,
     delete_car,
     retrieve_car,
-    retrieve_cars, update_car
+    retrieve_cars,
+    update_car,
+    retrieve_cars_brands,
+    retrieve_car_on_brand, retrieve_generations_car
 )
 from app.server.models.car import (
     ErrorResponseModel,
@@ -16,14 +19,35 @@ from app.server.models.car import (
 
 router = APIRouter()
 
+# Список марок машин
+@router.get("/brands", response_description="Возвращает список марок.")
+async def get_cars_brands():
+    cars = await retrieve_cars_brands()
+    if cars:
+        return ResponseModel(cars, "Cars data retrieved successfully")
+    return ResponseModel(cars, "Empty list returned")
 
-@router.post("/", response_description="Car data added into the database")
-async def add_car_data(car: CarSchema = Body(...)):
-    car = jsonable_encoder(car)
-    new_car = await add_car(car)
-    return ResponseModel(new_car, "Car added successfully.")
+
+# Информация о машинах определенной марки
+@router.get("/{brand}", response_description="Возвращает список моделей машин определенной марки.")
+async def get_cars_on_brand(brand: str):
+    car = await retrieve_car_on_brand(brand)
+    if car:
+        return ResponseModel(car, "Car data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "Car doesn't exist.")
 
 
+# Информация о поколениях модели
+@router.get("/{brand}/{model}", response_description="Возвращает список поколений машин определенной модели и марки. \n"
+                                                     "(Если lastYearProduction = 0, значит машина выпускается "
+                                                     "по настоящее время)")
+async def get_generations_car(brand: str, model: str):
+    car = await retrieve_generations_car(brand, model)
+    if car:
+        return ResponseModel(car, "Car data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "Car doesn't exist.")
+
+# Информация о всех машинах
 @router.get("/", response_description="Cars retrieved")
 async def get_cars():
     cars = await retrieve_cars()
@@ -31,7 +55,7 @@ async def get_cars():
         return ResponseModel(cars, "Cars data retrieved successfully")
     return ResponseModel(cars, "Empty list returned")
 
-
+# Информация о машине по индексу
 @router.get("/{id}", response_description="Car data retrieved")
 async def get_car_data(id):
     car = await retrieve_car(id)
@@ -39,7 +63,14 @@ async def get_car_data(id):
         return ResponseModel(car, "Car data retrieved successfully")
     return ErrorResponseModel("An error occurred.", 404, "Car doesn't exist.")
 
+# Добавление новой машины
+@router.post("/", response_description="Car data added into the database")
+async def add_car_data(car: CarSchema = Body(...)):
+    car = jsonable_encoder(car)
+    new_car = await add_car(car)
+    return ResponseModel(new_car, "Car added successfully.")
 
+#Обновление инфы о машине
 @router.put("/{id}")
 async def update_car_data(id: str, req: UpdateCarModel = Body(...)):
     req = {k: v for k, v in req.dict().items() if v is not None}
