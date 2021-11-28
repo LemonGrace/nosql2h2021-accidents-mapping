698a5c059065f35983b03db08548c57131fc1762
Olya Averina <averinaos@inbox.ru>
2021-11-29 00:40:27 +0300

    Добавлено разрешение cors

--- app/server/app.py
+++ app/server/app.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.server.routes.car import router as CarRouter
 
 app = FastAPI()
+
+origins = [
+    "http://domainname.com",
+    "https://domainname.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(CarRouter, tags=["Car"], prefix="/car")
 
